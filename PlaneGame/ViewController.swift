25d25b66dc887a8bd36910837e1c739e53e977c8
Goodwin790 <75224239+Goodwin790@users.noreply.github.com>
2023-06-30 17:58:40 +0300

    Delete all force unwraps

--- PlaneGame/ViewController.swift
+++ PlaneGame/ViewController.swift
@@ -8,47 +8,49 @@
 import UIKit
 
 private extension CGFloat {
-    static let dangerBorder = 70.0
-    static let seam = 5.0
-    static let scale = 1.0
-    static let ufoStep = 1.0
+    static let dangerBorder: CGFloat = 70.0
+    static let seam: CGFloat = 5.0
+    static let scale: CGFloat = 1.0
+    static let ufoStep: CGFloat = 1.0
 }
 
 private extension TimeInterval {
-    static let interval = 0.005
+    static let interval: TimeInterval = 0.005
 }
 
 class ViewController: UIViewController {
-    
+
     private var planeIsCrash = false
-    private var planeView: PlaneView!
-    private var firstUfoView: UfoView!
-    private var secondUfoView: UfoView!
-    private var leftMountainsFirstImage: UIImageView!
-    private var leftMountainsSecondImage: UIImageView!
-    private var rightMountainsFirstImage: UIImageView!
-    private var rightMountainsSecondImage: UIImageView!    
-    private var panGestureRecognizer: UIPanGestureRecognizer!
-    private var shootGesture: UITapGestureRecognizer?
+    private var planeView: PlaneView?
+    private var firstUfoView: UfoView?
+    private var secondUfoView: UfoView?
+    private var leftMountainsFirstImage: UIImageView?
+    private var leftMountainsSecondImage: UIImageView?
+    private var rightMountainsFirstImage: UIImageView?
+    private var rightMountainsSecondImage: UIImageView?
+    private var panGestureRecognizer: UIPanGestureRecognizer?
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupLandscape()
         setupFlyingViews()
         setupPlaneGestureRecognizers()
-        
     }
-    
+
     private func setupPlaneGestureRecognizers() {
         panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(handlePan(_:)))
-        planeView.addGestureRecognizer(panGestureRecognizer)
-        shootGesture = UITapGestureRecognizer(target: self, action: #selector(shoot(_:)))
-        guard let shootGesture = shootGesture else { return }
-        planeView.addGestureRecognizer(shootGesture)
+        if let panGestureRecognizer = panGestureRecognizer {
+            planeView?.addGestureRecognizer(panGestureRecognizer)
+        }
+
+        let shootGesture = UITapGestureRecognizer(target: self, action: #selector(shoot(_:)))
+        planeView?.addGestureRecognizer(shootGesture)
     }
 
     @objc private func handlePan(_ gestureRecognizer: UIPanGestureRecognizer) {
+        guard let planeView = planeView else { return }
         let translation = gestureRecognizer.translation(in: view)
-        
+
         switch gestureRecognizer.state {
         case .began, .changed:
             planeView.moveBy(x: translation.x, y: translation.y)
@@ -57,112 +59,135 @@ class ViewController: UIViewController {
             break
         }
     }
-    
+
     @objc private func shoot(_ gestureRecognizer: UITapGestureRecognizer) {
+        guard let planeView = planeView else { return }
         let pulit = Pulit(planeView: planeView)
         view.addSubview(pulit)
         pulit.startAnimation()
     }
-    
+
     private func setupFlyingViews() {
         planeView = PlaneView(frame: view.bounds)
         firstUfoView = UfoView(frame: view.bounds)
         secondUfoView = UfoView(frame: view.bounds)
-        view.addSubview(planeView)
-        view.addSubview(firstUfoView)
-        view.addSubview(secondUfoView)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            self.animateUfo(ufoView: self.secondUfoView)
+        if let planeView = planeView, let firstUfoView = firstUfoView, let secondUfoView = secondUfoView {
+            view.addSubview(planeView)
+            view.addSubview(firstUfoView)
+            view.addSubview(secondUfoView)
+            DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                self.animateUfo(ufoView: secondUfoView)
+            }
+            animateUfo(ufoView: firstUfoView)
         }
-        animateUfo(ufoView: firstUfoView)
-        
     }
-    
+
     private func setupLandscape() {
         let mountainsFirstImage = UIImageView(image: UIImage(named: "Mountains"))
         mountainsFirstImage.frame = view.bounds
         view.addSubview(mountainsFirstImage)
-        
+        leftMountainsFirstImage = mountainsFirstImage
+
         let mountainSecondImage = UIImageView(image: UIImage(named: "Mountains"))
         mountainSecondImage.frame = view.bounds
         mountainSecondImage.frame.origin.y = -view.bounds.height
         mountainSecondImage.transform = CGAffineTransform(scaleX: .scale, y: -.scale)
         view.addSubview(mountainSecondImage)
-        
+        leftMountainsSecondImage = mountainSecondImage
+
         let landscapeFrame = CGRect(
             x: view.frame.origin.x + .dangerBorder,
             y: view.frame.origin.y,
             width: view.frame.width - .dangerBorder * 2,
             height: view.frame.height)
-        
+
         let landscapeFirstImage = UIImageView(image: UIImage(named: "Landscape"))
         landscapeFirstImage.frame = landscapeFrame
         view.addSubview(landscapeFirstImage)
-        
+        rightMountainsFirstImage = landscapeFirstImage
+
         let landscapeSecondImage = UIImageView(image: UIImage(named: "Landscape"))
         landscapeSecondImage.frame = landscapeFrame
         landscapeSecondImage.frame.origin.y = -view.bounds.height
         landscapeSecondImage.transform = CGAffineTransform(scaleX: .scale, y: -.scale)
         view.addSubview(landscapeSecondImage)
-        
+        rightMountainsSecondImage = landscapeSecondImage
+
         animateLandscape(landscapeFirstImage, landscapeSecondImage)
         animateLandscape(mountainsFirstImage, mountainSecondImage)
     }
-    
-    func animateLandscape(_ firstImage: UIImageView, _ secondImage: UIImageView){
-        let _ = Timer.scheduledTimer(withTimeInterval: .interval, repeats: true) { timer in
+
+    func animateLandscape(_ firstImage: UIImageView, _ secondImage: UIImageView) {
+        let timer = Timer.scheduledTimer(withTimeInterval: .interval, repeats: true) { [weak self] timer in
+            guard let self = self else {
+                timer.invalidate()
+                return
+            }
+
             self.isPlaneCrash()
             firstImage.frame.origin.y += .scale
             secondImage.frame.origin.y += .scale
-            if (firstImage.layer.presentation()?.frame.origin.y) == self.view.bounds.origin.y {
+
+            if let presentationOriginY = firstImage.layer.presentation()?.frame.origin.y,
+                presentationOriginY == self.view.bounds.origin.y {
                 secondImage.frame.origin.y = -self.view.frame.height + .seam
             }
-            if (secondImage.layer.presentation()?.frame.origin.y) == self.view.bounds.origin.y {
+
+            if let presentationOriginY = secondImage.layer.presentation()?.frame.origin.y,
+                presentationOriginY == self.view.bounds.origin.y {
                 firstImage.frame.origin.y = -self.view.frame.height + .seam
             }
+
             if self.planeIsCrash {
                 timer.invalidate()
             }
         }
+        RunLoop.current.add(timer, forMode: .common)
     }
-    
+
     func animateUfo(ufoView: UfoView) {
         let minXUfo = view.frame.minX + .dangerBorder
         let maxXUfo = view.frame.maxX - .dangerBorder - ufoView.frame.width
-        Timer.scheduledTimer(withTimeInterval: 0.005, repeats: true) {timer in
+        let timer = Timer.scheduledTimer(withTimeInterval: 0.005, repeats: true) { [weak self] timer in
+            guard let self = self else {
+                timer.invalidate()
+                return
+            }
+
             self.isUfoCrash(ufo: ufoView)
             ufoView.frame.origin.y += .ufoStep
-            if ufoView.layer.presentation()?.frame.origin.y ?? -.dangerBorder >= self.view.bounds.maxY {
+
+            if let presentationOriginY = ufoView.layer.presentation()?.frame.origin.y,
+                presentationOriginY >= self.view.bounds.maxY {
                 ufoView.frame.origin.y = self.view.frame.minY - .dangerBorder
-                ufoView.frame.origin.x = .random(
-                    in:
-                        minXUfo...maxXUfo)
+                ufoView.frame.origin.x = .random(in: minXUfo...maxXUfo)
             }
-            if ufoView.frame.intersects(self.planeView.frame.insetBy(dx: 0.0, dy: 20)) {
+
+            if ufoView.frame.intersects(self.planeView?.frame.insetBy(dx: 0.0, dy: 20) ?? CGRect.zero) {
                 self.planeIsCrash = true
             }
+
             if self.planeIsCrash {
                 timer.invalidate()
             }
         }
+        RunLoop.current.add(timer, forMode: .common)
     }
-    
+
     func isUfoCrash(ufo: UfoView) {
         for subView in view.subviews {
-            if let pulit = subView as? Pulit {
-                if pulit.frame.intersects(ufo.frame) {
-                    pulit.removeFromSuperview()
-                    ufo.frame.origin.y = -view.frame.height
-                }
+            if let pulit = subView as? Pulit, pulit.frame.intersects(ufo.frame) {
+                pulit.removeFromSuperview()
+                ufo.frame.origin.y = -view.frame.height
             }
         }
     }
 
     func isPlaneCrash() {
-        if planeView.frame.minX <= .dangerBorder || planeView.frame.maxX >= self.view.frame.maxX - .dangerBorder {
-            planeIsCrash = true
+        if let planeView = planeView {
+            if planeView.frame.minX <= .dangerBorder || planeView.frame.maxX >= self.view.frame.maxX - .dangerBorder {
+                planeIsCrash = true
+            }
         }
     }
-    
 }
-
